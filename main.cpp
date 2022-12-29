9d313981de1cb85fdcddb8fd34be075b668e395f
natalia otrombke <nataliaotrombke@gmail.com>
2022-12-29 22:04:44 +0100

    added headings 1 to 4

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <regex>
 using namespace std;
 int main() {
+    const regex h1_pattern = regex("#{1} (.+)");
+    const regex h2_pattern = regex("#{2} (.+)");
+    const regex h3_pattern = regex("#{3} (.+)");
+    const regex h4_pattern = regex("#{4} (.+)");
     const regex h5_pattern = regex("#{5} (.+)");
     const regex h6_pattern = regex("#{6} (.+)");
 
@@ -16,9 +20,12 @@ int main() {
 
     string with_h6_replaced = regex_replace(input_md, h6_pattern, "<h6>$1</h6>");
     string with_h5_replaced = regex_replace(with_h6_replaced, h5_pattern, "<h5>$1</h5>");
+    string with_h4_replaced = regex_replace(with_h5_replaced, h4_pattern, "<h4>$1</h4>");
+    string with_h3_replaced = regex_replace(with_h4_replaced, h3_pattern, "<h3>$1</h3>");
+    string with_h2_replaced = regex_replace(with_h3_replaced, h2_pattern, "<h2>$1</h2>");
+    string with_h1_replaced = regex_replace(with_h2_replaced, h1_pattern, "<h1>$1</h1>");
 
-
-    cout << with_h5_replaced <<  endl;
+    cout << with_h1_replaced <<  endl;
 
     return 0;
 }
