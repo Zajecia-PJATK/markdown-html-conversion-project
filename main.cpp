2a008d2783f65636150c982ba370d57a2d3cfb29
natalia otrombke <nataliaotrombke@gmail.com>
2022-12-31 23:28:44 +0100

    italics, bold and strikethrough

--- main.cpp
+++ main.cpp
@@ -9,14 +9,19 @@ int main() {
     const regex h5_pattern = regex("#{5} (.+)");
     const regex h6_pattern = regex("#{6} (.+)");
 
+    const regex em_pattern = regex("\\*(.+)\\*");
+    const regex del_pattern = regex("~~(.+)~~");
+    const regex strong_pattern = regex("\\*\\*(.+)\\*\\*");
+
     const string input_md =
-            "# Art competition for children from day care centres\n"
-            "## Comprehensive survey on the functioning of day care centres\n"
-            "### Report on the presentation of gifts at the Day Care Centre Przystań\n"
-            "#### Report on the presentation of gifts at the ZATOKA day care centre\n"
-            "##### The gift is a sense of security\n"
-            "###### Study visit from the USA\n"
-            "###### wsedfyhiolkijugfdsxcv\n";
+            "# *Art* competition for children from day care centres\n"
+            "## *Comprehensive survey on the functioning of day care centres*\n"
+            "### *Report on th*e presentation of gifts at the Day Care Centre Przystań\n"
+            "#### ~~Report~~ on the presentation of gifts at the ZATOKA day care centre\n"
+            "##### The gif*t is a sens*e of security\n"
+            "###### **St**udy visit from the USA\n"
+            "###### *wsedfyhiolki*jugfdsxcv\n"
+            "q**wert**y";
 
     string with_h6_replaced = regex_replace(input_md, h6_pattern, "<h6>$1</h6>");
     string with_h5_replaced = regex_replace(with_h6_replaced, h5_pattern, "<h5>$1</h5>");
@@ -25,7 +30,12 @@ int main() {
     string with_h2_replaced = regex_replace(with_h3_replaced, h2_pattern, "<h2>$1</h2>");
     string with_h1_replaced = regex_replace(with_h2_replaced, h1_pattern, "<h1>$1</h1>");
 
-    cout << with_h1_replaced <<  endl;
+    string with_strong_replaced = regex_replace(with_h1_replaced, strong_pattern, "<strong>$1</strong>");
+    string with_em_replaced = regex_replace(with_strong_replaced, em_pattern, "<em>$1</em>");
+    string with_del_replaced = regex_replace(with_em_replaced, del_pattern, "<del>$1</del>");
+
+
+    cout << with_del_replaced <<  endl;
 
     return 0;
 }
