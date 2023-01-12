42aa07329a93552f2c4697deecd85b2041413c16
natalia otrombke <nataliaotrombke@gmail.com>
2023-01-12 23:56:38 +0100

    added grid layout

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <regex>
 #include <QApplication>
 #include <QPushButton>
+#include <QGridLayout>
+#include <QTextEdit>
+#include <QLabel>
 
 using namespace std;
 
@@ -10,11 +13,33 @@ using namespace std;
 #include "markdown2html.h"
 #include "html2markdown.h"
 
+
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+
+    auto *load_button = new QPushButton("Load");
+    auto *save_button = new QPushButton("Save");
+    auto *swap_button = new QPushButton("Swap");
+    auto *input_text = new QTextEdit("left");
+    auto *output_text = new QTextEdit("right");
+    auto *input_label = new QLabel("HTML");
+    auto *output_label = new QLabel("MD");
+
+    input_label->setAlignment(Qt::AlignCenter);
+    output_label->setAlignment(Qt::AlignCenter);
+
+    auto *window = new QWidget();
+    auto layout = new QGridLayout(window);
+
+    layout->addWidget(load_button, 0, 0);
+    layout->addWidget(save_button, 0,5);
+    layout->addWidget(swap_button, 0, 2, 1, 2);
+    layout->addWidget(input_text, 1, 0, 4, 3);
+    layout->addWidget(output_text, 1, 3, 4, 3);
+    layout->addWidget(input_label, 0, 1);
+    layout->addWidget(output_label, 0, 4);
+
+    window->show();
     return QApplication::exec();
 }
 
