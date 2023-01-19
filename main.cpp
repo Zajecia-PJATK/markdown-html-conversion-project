8658b8728e09d2d7e23e1860336cd0ab15cbc534
natalia otrombke <nataliaotrombke@gmail.com>
2023-01-19 23:21:07 +0100

    opening a file and saving the file

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <QTextEdit>
 #include <QLabel>
 #include <QObject>
+#include <QFileDialog>
 
 using namespace std;
 
@@ -28,7 +29,7 @@ int main(int argc, char *argv[]) {
     auto *input_label = new QLabel("HTML");
     auto *output_label = new QLabel("MD");
     auto *live_preview_text_edit = new QTextEdit();
-
+//*wskaźnik
     input_label->setAlignment(Qt::AlignCenter);
     output_label->setAlignment(Qt::AlignCenter);
     input_text_edit->setPlaceholderText("Start typing here");
@@ -47,8 +48,8 @@ int main(int argc, char *argv[]) {
 
     //to obsługuje zamianę guzika swap
     QObject::connect(swap_button, &QPushButton::clicked, [&is_input_html, &input_label, &output_label]() {
-        is_input_html = !is_input_html;
-        input_label->setText(is_input_html ? "HTML" : "MD");
+        is_input_html = !is_input_html;//zmienna ma się równać jej przeciwieństwu
+        input_label->setText(is_input_html ? "HTML" : "MD");//inpit_label = tekst, nie zmienna waryości, settext = ustawiać tekst, -> wyciąganie
         output_label->setText(is_input_html ? "MD" : "HTML");
     });
 
@@ -61,6 +62,36 @@ int main(int argc, char *argv[]) {
          live_preview_text_edit->setText(QString::fromStdString(markdown2html(output_text)));
     });
 
+    //obsługa guzika load
+    QObject::connect(load_button, &QPushButton::clicked, [&window, &is_input_html, &input_text_edit](){
+        auto fileName = QFileDialog::getOpenFileName(window, "Open File", nullptr, is_input_html ? "Source files (*.html)" : "Source files (*.md)");
+
+        if( fileName == nullptr){
+            return;
+        }
+
+        QFile file(fileName);
+        file.open(QFile::ReadOnly | QFile::Text);
+        QTextStream contents(&file);
+
+        input_text_edit->setText(contents.readAll());
+    });
+
+    // obsługa guzika save
+    QObject::connect(save_button, &QPushButton::clicked,[&window, &is_input_html, &output_text_edit](){
+        auto fileName = QFileDialog::getSaveFileName(window, "Save File", is_input_html ? "file.md" : "file.html", is_input_html ? "Source file (*.md)" : "Source file (*.html)");
+
+        if ( fileName == nullptr){
+            return;
+        }
+        QFile file(fileName);
+        file.open(QFile::WriteOnly | QFile::Text);
+
+        QTextStream contents(&file);
+
+        contents << output_text_edit->toPlainText();
+        file.close();
+    });
 
     window->show();
     return QApplication::exec();
